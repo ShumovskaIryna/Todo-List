1589f0d41894a3f439df9c690cf83df11ab58211
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-06-29 19:11:49 +0300

    statistic page

--- src/views/StatisticView.vue
+++ src/views/StatisticView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>Statistic</h1>
+  <div class="statistic">
+
+    <div class="list" id="todo-list">
+      You have to do {{todoStore.taskIsNotDone.length}} tasks
+    </div>
+
+    <div class="list" id="todo-list">
+      You have already done {{todoStore.taskIsDone.length}} tasks
+    </div>
+
   </div>
 </template>
+
+<script>
+import { useTodoStore } from '@/stores/todoStore'
+
+export default {
+  setup() {
+	const todoStore = useTodoStore()
+	return { todoStore }
+  }
+}
+
+</script>
