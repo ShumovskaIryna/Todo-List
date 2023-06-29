08dc54b5dc13f9eec809e312d72178ccba21d22b
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-06-29 15:31:43 +0300

    install icon

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import App from './App.vue'
 import router from './router'
@@ -12,3 +13,5 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.component('font-awesome-icon', FontAwesomeIcon)
