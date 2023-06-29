b445f4dd321818c53c07664bf044adfa5ba2d561
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-06-29 19:10:59 +0300

    filter todos on done & notDone

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -6,6 +6,14 @@ export const useTodoStore = defineStore('todoList', {
         {id: 1, title: "buy some fruit", isDone: false },
         {id: 2, title: "buy some milk", isDone: true }
       ]
-    })
+    }),
+    getters: {
+      taskIsDone(){
+        return this.tasks.filter(t => t.isDone)
+      },
+      taskIsNotDone(){
+        return this.tasks.filter(t => !t.isDone)
+      },
+    },
   },
 )
